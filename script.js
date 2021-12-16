56a8a4c34da0413f129d4309808fef3bb4d62486
abdulloh <abdulloh@gmail.com>
2021-12-16 19:49:56 +0500

    js.start

---
+++ script.js
@@ -0,0 +1,13 @@
+var name = prompt ('Ismingizni kiriting:') 
+console.log('Sizning ismingiz: ' + name );
+var age = prompt('Yoshingizni kiriting:' )
+console.log('Sizning yoshingiz: '+age ); 
+var answer = prompt('Misolni yeching: 10+5= ? ')
+console.log('sizning javobingiz : '+answer + "    to'g'ri javob: " +(10+5))
+var answer = prompt('Misolni yeching: 10-5= ? ')
+console.log('sizning javobingiz : '+answer + "    to'g'ri javob: " +(10-5)) 
+var answer = prompt('Misolni yeching: 10*5= ? ')
+console.log('sizning javobingiz : '+answer + "    to'g'ri javob: "+10*5 )
+var answer = prompt('Misolni yeching: 10/5= ? ')
+console.log('sizning javobingiz : '+answer + "    to'g'ri javob: "+ 10/5 ) 
+alert("Natijani ko'rish uchun konsolni oching")
